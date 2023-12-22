a89c0a642009549efb8b39a9662793c846ba98c5
Darius <dariuskumza@gmail.com>
2023-12-22 16:01:37 +0200

    server rdy

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+
+const app = express();
+const port = 3003;
+
+// # MIDDLEWARE #
+
+app.use(morgan("dev"));
+app.use(cors()); // to fix cors error
+app.use(express.json()); //to read req.body in json
+
+// # ROUTES #
+
+// Home route
+app.get("/", (req, res) => {
+   res.send("Hello from Express");
+});
+
+app.listen(port, () => {
+   console.log(`server is running http://localhost:${port}`);
+});
