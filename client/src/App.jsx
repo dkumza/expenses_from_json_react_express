2e9c59066aef34aa784c61826f5c9195c89484ef
Darius <dariuskumza@gmail.com>
2023-12-23 12:16:18 +0200

    Add New component to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import { AddNewButton } from "./components/AddNewButton";
 import { ExpProvider } from "./components/ExpContext";
 import { Head } from "./components/Head";
 import { Main } from "./components/Main";
+import { New } from "./components/New";
 
 function App() {
    return (
       <ExpProvider>
          <div className="main-wrap container mx-auto 2xl:w-1/5 md:w-2/5 min-h-screen relative">
             <Head />
+            <New />
             <Main />
-            <AddNewButton />
+            {/* <AddNewButton /> */}
          </div>
       </ExpProvider>
    );
