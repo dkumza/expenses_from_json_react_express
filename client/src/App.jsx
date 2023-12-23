24ca839378aa51f7c4503f0db89cf40b4ef407d0
Darius <dariuskumza@gmail.com>
2023-12-23 09:53:02 +0200

    client up

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { InputModal } from "./components/InputModal";
+
+function App() {
+   return <div className="main-wrap container mx-auto"></div>;
+}
+
+export default App;
